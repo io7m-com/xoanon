9dd9560b5010d37e531f77cd41be97b9da463f9c
Mark Raynsford <code@io7m.com>
2024-05-12 10:02:04 +0000

    Use junit BOM and org.junit.platform.launcher
    This allows the test suite to run without needing a workaround
    to open modules.
    
    Thanks @sormuras
    
    Affects: https://github.com/junit-team/junit5/issues/3810

--- com.io7m.xoanon.tests/src/main/java/module-info.java
+++ com.io7m.xoanon.tests/src/main/java/module-info.java
@@ -24,6 +24,7 @@ open module com.io7m.xoanon.tests
   requires org.junit.jupiter.engine;
   requires org.junit.platform.commons;
   requires org.junit.platform.engine;
+  requires org.junit.platform.launcher;
 
   requires com.io7m.xoanon.commander.api;
   requires com.io7m.xoanon.commander;
